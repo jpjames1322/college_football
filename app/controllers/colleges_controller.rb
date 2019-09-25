69151f98774a0f0164f5bf0465d8ec4fd049acf4
jared-phillips <jp.james0927@gmail.com>
2019-09-25 22:58:53 +0000

    update

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,19 +1,26 @@
 class CollegesController < ApplicationController
   def index
+    @colleges = College.all
   end
+
   def show
   end
+
   def new
     @colleges = College.new
   end
+
   def create
-    College.create(college_params)
-    if @college.invalid?
+    College.create(colleges_params)
+    if @colleges.invalid?
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-  end
+    end
     redirect_to root_path
   end
+
   private
+  
   def college_params
     params.require(:college).permit(:team, :mascot)
+  end
 end
--- app/models/college.rb
+++ app/models/college.rb
@@ -1,3 +1,4 @@
 class College < ApplicationRecord
-  validates :team, :mascot, presence: true, length: {maximum: 100, minimum: 3}
+  validates :team, presence: true, length: {maximum: 100, minimum: 3}
+  validates :mascot, presence: true, length: {maximum: 50, minumum: 3}
 end
